07e2cce3c9ceef7cec45582dc46aaa6ff81f1851
AmyB <31877717+amyboeren@users.noreply.github.com>
2021-10-22 18:11:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//Comment, dit wordt niet meegenomen in JavaScript.
+//let name='Mosh';
+//console.log(name);
+
+//Hoofdlettergevoelig
+//Kan geen spatie of hyphen bevatten
+//Kan niet starten met een nummer
+//Moet iets betekenen
+//eerste woord met kleine letter en tweede woord met hoofdletter
+
+//let name = 'Mosh'; //String Literal
+//let age = 30; //Number Literal
+//let isApproved = true; //Boolean Literal
+//let firstName = undefined;
+//let lastName = null;
+//als we de variabele leeg willen laten
+
+console.log("Hello Winc Academy");
+let name="Amy";
+console.log(name);
+console.log(8)
+console.log("44")
+//10-10=
+console.log(0)
+//4*4=
+console.log(16)
+//4/4=
+console.log(1)
+//10% 100=
+console.log(10)
+
+let age=39;
+console.log(age);
+let age="39";
+console.log(age);
